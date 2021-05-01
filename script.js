c6ef70a3ee5c3989aa452dec6c234e4d2b7a518f
cakloeppel <c.a.kloeppel@gmx.de>
2021-05-01 19:19:43 +0200

    Add content switching thingy

---
+++ script.js
@@ -0,0 +1,27 @@
+function showSection(section) {
+  if (!section) section = "default";
+  let sections = ["default", "a", "b"];
+  for (let s_name of sections) {
+    let s = "#content-" + s_name;
+    if (s_name === section)
+      $( s ).show();
+    else
+      $( s ).hide();
+  }
+}
+
+function updateSection() {
+  let anchor = this.window.location.hash.substr(1);
+  showSection(anchor);
+}
+
+$( document ).ready(() => {
+  updateSection();
+
+  $( "a" ).click((ev) => {
+    let section = ev.target.href.split("#")[1];
+    showSection(section);
+  });
+
+  $( ".content" ).show();
+});
